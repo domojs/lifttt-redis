10618fbbb6ebef4f3dcca6788a28c2919c3c29fb
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2016-03-16 21:34:57 +0100

    polling fix

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ module.exports={
                 var client=$('../modules/db/node_modules/redis').createClient(fields.port || 6379, fields.host || 'localhost');
                 var checker=function(){
                     client[fields.command](fields.args, function(err, result){
+                        //console.log(fields.command, fields.args, ':', result);
                         if(err)
                             console.log(err);
                         else if(result==fields.value)
@@ -37,11 +38,105 @@ module.exports={
                     })
                 };
                 checker();
-                setTimeout(checker, fields.poll);
+                setInterval(checker, fields.poll);
+            }
+        },
+        {
+            name:"compare",
+            fields:[{name:"command", displayName:"The command to run"}, {name:"args", displayName:"arguments"},{name:"operator", displayName:"Operator"}, {name:"poll", displayName:"Polling interval"}, {name:"value", displayName:"Value to compare"}, {name:"host", displayName:"The redis host"}, {name:"port", displayName:"The redis port"}],
+            when:function(fields, callback){
+                var client=$('../modules/db/node_modules/redis').createClient(fields.port || 6379, fields.host || 'localhost');
+                var oldResult=false;
+                var checker=function(){
+                    client[fields.command](fields.args, function(err, result){
+                        console.log(fields.command, fields.args, ':', result, fields.operator, fields.value);
+                        if(err)
+                            console.log(err);
+                        else
+                        {
+                            switch(fields.operator)
+                            {
+                                case '>=':
+                                    if(!oldResult && result>=fields.value)
+                                    {
+                                        oldResult=true;
+                                        console.log('true')
+                                        callback({value:result});
+                                    }
+                                    else if(result<fields.value)
+                                        oldResult=false;
+                                    break;                                    
+                                case '<=':
+                                    if(!oldResult && result<=fields.value)
+                                    {
+                                        oldResult=true;
+                                        console.log('true')
+                                        callback({value:result});
+                                    }
+                                    else if(result>fields.value)
+                                        oldResult=false;
+                                    break;                                    
+                                case '>':
+                                    if(!oldResult && result>fields.value)
+                                    {
+                                        oldResult=true;
+                                        console.log('true')
+                                        callback({value:result});
+                                    }
+                                    else if(result<=fields.value)
+                                        oldResult=false;
+                                    break;                                    
+                                case '<':
+                                    if(!oldResult && result<fields.value)
+                                    {
+                                        oldResult=true;
+                                        console.log('true')
+                                        callback({value:result});
+                                    }
+                                    else if(result>=fields.value)
+                                        oldResult=false;
+                                    break;                                    
+                                case '!=':
+                                    if(!oldResult && result!=fields.value)
+                                    {
+                                        oldResult=true;
+                                        console.log('true')
+                                        callback({value:result});
+                                    }
+                                    else if(result==fields.value)
+                                        oldResult=false;
+                                    break;                                    
+                                case '=':
+                                default:
+                                    if(!oldResult && result==fields.value)
+                                    {
+                                        oldresult=true;
+                                        console.log('true')
+                                        callback({value:result});
+                                    }
+                                    else if(result!=fields.value)
+                                        oldResult=false;
+                                    break;                                    
+                            }
+                        }
+                    })
+                };
+                checker();
+                setInterval(checker, fields.poll);
             }
         }
     ],
     "actions":
     [
+        {
+            name:"exec",
+            fields:[{name:"command", displayName:"The command to run"}, {name:"args", displayName:"arguments"}, {name:"host", displayName:"The redis host"}, {name:"port", displayName:"The redis port"}],
+            when:function(fields){
+                var client=$('../modules/db/node_modules/redis').createClient(fields.port || 6379, fields.host || 'localhost');
+                client[fields.command](fields.args, function(err, result){
+                    
+                });
+            }
+        }
     ]
 }; 
